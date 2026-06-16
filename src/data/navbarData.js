19b36c528f6bce1fe139d2c1cbb7bec4a3a00b1f
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-06-16 16:29:28 -0400

    refactor(navbar): update links, icons and css styling.

--- src/data/navbarData.js
+++ src/data/navbarData.js
@@ -1,8 +1,10 @@
-import { FiUser, FiActivity, FiTerminal, FiLayers, FiFileText } from "react-icons/fi";
+import { FiUser, FiGitBranch, FiTerminal, FiLayers} from "react-icons/fi";
+import { VscHome } from "react-icons/vsc";
 
 export const navigationItems = [
-  {linkName: 'Home', href: '#home', icon: FiUser},
-  {linkName: 'Experience', href: '#experience', icon: FiActivity},
+  {linkName: 'Home', href: '#home', icon: VscHome},
+  {linkName: 'About', href: '#about', icon: FiUser},
+  {linkName: 'Experience', href: '#experience', icon: FiGitBranch},
   {linkName: 'Projects', href: '#projects', icon: FiTerminal},
   {linkName: 'Toolkit', href: '#toolkit', icon: FiLayers}
 ];
