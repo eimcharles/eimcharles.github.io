6f3e17fd626c194577dc3778c2000762a1628a1c
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-06-16 14:23:35 -0400

    feat(hero): add responsive hero component with animated introduction text and social icons.

---
+++ src/data/heroData.js
@@ -0,0 +1,6 @@
+export const heroData = [
+    "Welcome to my personal portfolio.",
+    "I am a Software Engineering Graduate.", 
+    "I enjoy building user-friendly software.", 
+    "Let's connect!", 
+  ];
