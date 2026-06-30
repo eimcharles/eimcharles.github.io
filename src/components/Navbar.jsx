7e7b43afe859b3bb695fed63b8419d5f61124e99
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-06-30 14:53:41 -0400

    chore(navbar): update navbar background and padding.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     <nav className="fixed top-4 left-0 right-0 z-50 px-4 w-full select-none">
       <div className="flex flex-row justify-center items-center px-2.5 py-2 
                       sm:px-4 sm:py-3 gap-1 sm:gap-3 max-w-fit mx-auto
-                      font-satoshi backdrop-blur-sm rounded-xl">
+                      font-satoshi backdrop-blur-sm bg-[#ffffff]/30 rounded-xl">
                         
       {navigationItems.map((item) => {
         const isActive = activeTab === item.href;
@@ -19,12 +19,12 @@ const Navbar = () => {
             key={item.linkName} 
             href={item.href} 
             onClick={() => setActiveTab(item.href)}
-            className="relative px-3 py-1.5 sm:px-4 sm:py-2 text-xs sm:text-sm tracking-wide text-stone-900 rounded-full transition-colors duration-200"
-          >
+            className="relative px-2 py-1.5 sm:px-4 sm:py-2 text-xs sm:text-sm tracking-wide text-stone-900 
+                       rounded-full transition-colors duration-200">
               {isActive && (
                 <motion.span 
                   layoutId="activeTab" 
-                  className="absolute inset-0 rounded-xl z-0 black backdrop-blur-xl bg-[#99a39d]/20"
+                  className="absolute inset-0 rounded-xl z-0 black backdrop-blur-xl bg-[#667a70]/20"
                   transition={{ type: "spring", stiffness: 380, damping: 30 }}
                 />
               )}
