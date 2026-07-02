312fc4688a0a5597408ff7f15e7f4f0392a60da6
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-07-02 16:21:59 -0400

    chore(background): remove color transitions from timeline components.

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -26,16 +26,16 @@ const Background = () => {
                   <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-2 pb-1">
                     <div className="flex flex-col gap-2 [&_svg]:transition-colors [&_svg]:duration-500 [&_svg]:shrink-0">
                       <h3 className="font-chillax lg:text-xl font-black tracking-tight text-stone-900 flex items-center gap-1.5">
-                        <backgroundIcons.role className="group-hover:text-[#afafaf]"/>{item.role}
+                        <backgroundIcons.role className="text-[#afafaf]"/>{item.role}
                       </h3>
                       <p className="font-satoshi text-xs lg:text-sm font-medium text-stone-900 flex items-center gap-1.5">
-                        <backgroundIcons.calendar className="group-hover:text-[#1e63ca]" /> {item.duration} 
+                        <backgroundIcons.calendar className="text-[#1e63ca]" /> {item.duration} 
                       </p>
                       <p className="font-satoshi text-xs lg:text-sm font-medium text-stone-900 flex items-center gap-1.5">
-                        <backgroundIcons.company className="group-hover:text-[#c71717]" /> {item.organization} 
+                        <backgroundIcons.company className="text-[#c71717]" /> {item.organization} 
                       </p>
                       <p className="font-satoshi text-xs lg:text-sm text-stone-900 flex items-center gap-1.5 italic">
-                        <backgroundIcons.growth className="group-hover:text-[#209b3e]" />{item.summary}
+                        <backgroundIcons.growth className="text-[#209b3e]" />{item.summary}
                       </p>
                     </div>
                   </div>
@@ -44,7 +44,7 @@ const Background = () => {
                     {item.contributions.map((contribution) => (
                       <div key={contribution.id} className="flex justify-start gap-1">
                         <backgroundIcons.arrow className="text-lg lg:text-xl shrink-0 select-none"/>
-                        <p className="text-justify group-hover:text-stone-500 transition-colors duration-500">{contribution.text}</p>
+                        <p className="text-justify text-stone-500 transition-colors duration-500">{contribution.text}</p>
                       </div>
                     ))}
                   </div>                  
