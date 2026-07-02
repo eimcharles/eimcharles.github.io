d82b9324a8b7c41f533802941c151c1d887ecc70
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-07-02 16:26:39 -0400

    refactor(projects): update project card layout and styling.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,67 +9,58 @@ const Projects = () => {
           Featured Projects
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-8 gap-6 w-full">
+        <div className="grid grid-cols-1 md:grid-cols-8 gap-4 w-full">
           {projectsData.map((project) => (
             <div key={project.id} className={`group ${project.gridSpan} w-full flex flex-col`}>
               
               <div className="flex flex-col gap-4 overflow-hidden rounded-2xl border border-stone-200 
-                              hover:scale-[0.99] transition-all duration-500 bg-[#ffffff]">  
-                <div className="flex flex-col gap-4 pt-4 px-6 sm:pt-8 sm:px-8">
+                              hover:scale-[0.99] transition-all duration-500 hover:bg-[#ffffff] hover:shadow-none bg-stone-200/20 shadow-xl">  
+                <div className="flex flex-col gap-5 pt-4 px-6 sm:pt-8 sm:px-8">
                   
-                  <div className="flex flex-row justify-between items-center gap-2">
+                  <div className="flex flex-row justify-between gap-4">
                     <h3 className="font-chillax lg:text-xl font-black tracking-tight text-stone-900">
                       {project.title}
-                    </h3> 
+                    </h3>   
 
+                    <div className="flex flex-row justify-end gap-8 font-chillax text-xs lg:text-sm">
+                      {project.demoUrl && (
+                        <a href={project.demoUrl} target="_blank" rel="noreferrer" 
+                        className="flex items-center gap-1 text-stone-900 transition-all duration-300 hover:translate-x-1"
+                          title={`Live Link for ${project.title}`}>
+                          <projectsIcons.demoUrl className="text-xs lg:text-lg text-[#1e63ca]" />
+                          <p className="text-xs lg:text-sm font-satoshi text-stone-500">{project.demo}</p>
+                        </a>
+                      )}
+                      <a href={project.githubUrl} target="_blank" rel="noreferrer" title={`${project.title} Repository`}
+                      className="flex items-center gap-2 text-stone-900 transition-all duration-300 hover:translate-x-1">
+                        <p className="text-xs lg:text-sm font-satoshi text-stone-500">Github</p>
+                        <projectsIcons.github className="text-xs lg:text-lg text-[#F05032] transition-colors duration-500"/>
+                      </a>
+                    </div> 
+                  </div>  
+
+                  <div className="flex flex-row justify-between items-start gap-4">
+                    <p className="font-satoshi text-xs lg:text-sm font-black text-stone-500">
+                      {project.type}
+                    </p>
+                    <div className="flex flex-row gap-4">
                     {project.languages && (
-                      <div className="flex flex-row items-center gap-3">
+                      <div className="flex flex-row justify-end gap-4 w-full">
                         {project.languages.map((item) => {
                           const Icon = item.icon;
                           return (
-                            <div key={item.id} className="flex items-center" title={item.name}>
+                            <div key={item.id} className="flex justify-between" title={item.name}>
                               <Icon className={`text-xs lg:text-lg ${item.color} transition-colors duration-500`} />
                             </div>
                           );
                         })}
                       </div>
-                    )}    
-                  </div>  
-
-                  <div className="flex flex-row justify-between items-center border-b border-stone-300 pb-1">
-                    <p className="font-satoshi text-xs lg:text-sm font-black text-stone-900">
-                      {project.type}
-                    </p>
-                    
-                    <div className="flex justify-end items-center gap-4 font-chillax text-xs lg:text-sm">
-                      {project.demoUrl && (
-                        <a href={project.demoUrl} target="_blank" rel="noreferrer" 
-                        className="flex items-center gap-2 text-stone-900 transition-all duration-300 hover:translate-x-1"
-                          title={`Live Link for ${project.title}`}>
-                          <p className="text-xs lg:text-sm font-satoshi text-stone-900 group-hover:text-stone-500 transition-colors duration-500">
-                            {project.demo}
-                          </p>
-                          <projectsIcons.demoUrl className="text-xs lg:text-lg text-stone-900 group-hover:text-[#1e63ca] transition-colors duration-500" />
-                        </a>
-                      )}
-                      
-                      <a href={project.githubUrl} target="_blank" rel="noreferrer" title={`${project.title} Repository`}
-                      className="flex items-center gap-2 text-stone-900 transition-all duration-300 hover:translate-x-1">
-                        <p className="text-xs lg:text-sm font-satoshi text-stone-900 group-hover:text-stone-500 transition-colors duration-500">
-                          Github
-                        </p>
-                        <projectsIcons.github className="text-xs lg:text-lg text-stone-900 group-hover:text-[#F05032] transition-colors duration-500"/>
-                      </a>
-                    </div>
-                  </div> 
-                  
-                  <div className="flex flex-row justify-start items-start gap-1">
-                    <projectsIcons.arrow className="text-lg text-[#afafaf] shrink-0" />
-                    <p className="font-satoshi tracking-tight font-black text-xs lg:text-sm text-stone-900 text-justify 
-                                  leading-relaxed group-hover:text-stone-500 transition-colors duration-500">
-                      {project.description}
-                    </p>
-                  </div>
+                    )} 
+                    </div> 
+                  </div>      
+                  <p className="font-satoshi font-black text-xs lg:text-sm text-justify leading-relaxed text-stone-500">
+                    {project.description}
+                  </p>          
                 </div>
 
                 <div className="w-full aspect-video mt-auto overflow-hidden">
@@ -87,8 +78,7 @@ const Projects = () => {
             <p className="text-xs lg:text-sm group-hover:text-stone-500 transition-colors duration-500">
               Explore more projects
             </p>
-            <projectsIcons.github className="text-xs lg:text-lg text-stone-900 transition-all duration-300 
-                                           group-hover:text-[#F05032] group-hover:translate-x-1" />
+            <projectsIcons.github className="text-xs lg:text-lg transition-all duration-300 text-[#F05032] group-hover:translate-x-1" />
           </a>
         </div>
 
