5faae91f2aac59516089564c5100c139c9582555
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-07-02 16:34:31 -0400

    chore(app): remove unused components imports for build and deployment.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
-import Toolkit from './components/Toolkit';
 import Background from './components/Background';
 import Projects from './components/Projects';
-import Feedback from './components/Feedback';
 import Footer from './components/Footer';
 
 function App() {
