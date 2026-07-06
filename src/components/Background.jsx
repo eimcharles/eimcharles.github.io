10be60651a9c5b87eb27c29cc265c1d24a1da1f3
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-07-06 13:18:43 -0400

    chore(background): update styling for mobile and component data.

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,9 +3,9 @@ import { backgroundData, backgroundIcons } from '../data/backgroundData';
 
 const Background = () => {
   return (
-    <section id="background" className="w-full px-6 py-16 md:py-24 select-none overflow-hidden bg-[#ffffff]">
+    <section id="background" className="w-full px-6 py-36 select-none overflow-hidden bg-[#ffffff]">
       <div className="flex flex-col w-full max-w-6xl mx-auto gap-8 md:gap-12">
-        <h2 className="text-3xl md:text-5xl font-black font-satoshi tracking-tighter text-stone-900 leading-none text-center">
+        <h2 className="text-4xl md:text-5xl font-black font-satoshi tracking-tighter text-stone-900 leading-none text-center">
           My Background
         </h2>
 
@@ -20,31 +20,31 @@ const Background = () => {
                 <div className="hidden md:block absolute left-1/2 top-10 w-1 h-2/3 rounded-4xl bg-stone-400/20 group-hover:bg-stone-900
                                 group-hover:border-none -translate-x-1/2 transition-all duration-300 group-hover:scale-50 z-10" />
 
-                <div className="w-full md:w-[48%] rounded-2xl p-6 sm:p-8 flex flex-col transition-all duration-500
+                <div className="w-full md:w-[48%] rounded-2xl p-4 sm:p-6 flex flex-col transition-all duration-500
                  hover:bg-[#ffffff] hover:shadow-none bg-stone-200/20 shadow-xl">
                   
-                  <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-2 pb-1">
-                    <div className="flex flex-col gap-2 [&_svg]:transition-colors [&_svg]:duration-500 [&_svg]:shrink-0">
-                      <h3 className="font-chillax lg:text-xl font-black tracking-tight text-stone-900 flex items-center gap-1.5">
+                  <div className="flex flex-col sm:flex-row sm:items-center justify-between">
+                    <div className="flex flex-col gap-2 [&_svg]:transition-colors [&_svg]:duration-500 [&_svg]:shrink-0 w-full">
+                      <h3 className="font-chillax text-lg tracking-tight text-stone-900 flex justify-start items-center gap-1">
                         <backgroundIcons.role className="text-[#afafaf]"/>{item.role}
                       </h3>
-                      <p className="font-satoshi text-xs lg:text-sm font-medium text-stone-900 flex items-center gap-1.5">
-                        <backgroundIcons.calendar className="text-[#1e63ca]" /> {item.duration} 
-                      </p>
-                      <p className="font-satoshi text-xs lg:text-sm font-medium text-stone-900 flex items-center gap-1.5">
+                      <p className="font-satoshi text-sm text-stone-900 flex justify-start gap-1.5">
                         <backgroundIcons.company className="text-[#c71717]" /> {item.organization} 
                       </p>
-                      <p className="font-satoshi text-xs lg:text-sm text-stone-900 flex items-center gap-1.5 italic">
+                      <p className="font-satoshi text-sm text-stone-900 flex  justify-start gap-1.5">
+                        <backgroundIcons.calendar className="text-[#1e63ca]" /> {item.duration} 
+                      </p>
+                      <p className="font-satoshi text-sm text-stone-900 flex items-center gap-1.5 italic pb-2">
                         <backgroundIcons.growth className="text-[#209b3e]" />{item.summary}
                       </p>
                     </div>
                   </div>
 
-                  <div className="flex flex-col gap-1 font-satoshi text-xs lg:text-sm text-stone-900 leading-relaxed pt-1">
+                  <div className="flex flex-col gap-2 font-satoshi text-stone-900 leading-relaxed">
                     {item.contributions.map((contribution) => (
                       <div key={contribution.id} className="flex justify-start gap-1">
                         <backgroundIcons.arrow className="text-lg lg:text-xl shrink-0 select-none"/>
-                        <p className="text-justify text-stone-500 transition-colors duration-500">{contribution.text}</p>
+                        <p className="text-justify text-stone-500 text-sm transition-colors duration-500">{contribution.text}</p>
                       </div>
                     ))}
                   </div>                  
--- src/data/backgroundData.js
+++ src/data/backgroundData.js
@@ -17,7 +17,7 @@ export const backgroundData = [
     role: "B.Eng Software Engineering",
     organization: "Concordia University",
     duration: "Jan 2019 - Oct 2025",
-    summary: "Bachelors of Software Engineering.",
+    summary: "Bachelors of Software Engineering",
     contributions: [
       {id: "concordia-c1" , text: "Member of C-Edge for Co-Operative Education."}
     ]
@@ -27,7 +27,7 @@ export const backgroundData = [
     role: "Software Developer I",
     organization: "Intact Insurance",
     duration: "Sept 2023 - Aug 2024",
-    summary: "Permanent Part-time with Contact Billing Team.",
+    summary: "Permanent Part-time - Contact Billing Team",
     contributions: [
       {id: "intact-c1" , text: "Developed automated reconciliation tools achieving 100% visibility of inter-agency fund transfers."},
       {id: "intact-c2" , text: "Supported QA processes by conducting acceptance and manual tests."},
@@ -39,7 +39,7 @@ export const backgroundData = [
     role: "Software Developer Intern",
     organization: "Intact Insurance",
     duration: "May 2023 - Sept 2023",
-    summary: "Second internship with Contact Billing Team.",
+    summary: "Second internship - Contact Billing Team",
     contributions: [
       {id: "intact-c4" , text: "Modernized billing services to a contract-first approach with OpenAPI."},
       {id: "intact-c5" , text: "Strengthened the reliability of services through payload validation."},
@@ -51,7 +51,7 @@ export const backgroundData = [
     role: "Software Developer Intern",
     organization: "Intact Insurance",
     duration: "Jan 2023 - May 2023", 
-    summary: "First internship with Contact Billing Team.",
+    summary: "First internship - Contact Billing Team",
     contributions: [
       {id: "intact-c7" , text: "Refactored service configurations, eliminating dependencies and plugins."},
       {id: "intact-c8" , text: "Contributed to the refinement of the team's engineering backlogs."},
