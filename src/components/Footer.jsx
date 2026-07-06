871e9ebde6a6ce6cf0e9cb073c7c1c444e04fa84
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-07-06 14:11:02 -0400

    chore(footer): update footer layout and add design credit.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,15 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full py-16 bg-stone-300/20 rounded-2xl px-6 select-none font-satoshi font-black text-stone-900 text-xs lg:text-sm">
-      <p className="flex justify-center items-center gap-1">          
-        <footerIcons.copyright className="text-md"/> {currentYear} Charles Eimer - All rights reserved.
-        </p>  
+    <footer className="w-full py-16 bg-[#fffffff] rounded-2xl px-6 select-none font-satoshi font-black text-stone-900 text-xs lg:text-sm">
+      <div className='flex flex-col md:flex-row justify-center md:justify-around gap-4'>
+        <p className="flex justify-center items-center gap-1.5 font-satoshi text-sm">
+          Designed and <footerIcons.code className="text-[#209b3e]"/> with <footerIcons.heart className="fill-[#d40f78] text-[#e11515]"/>
+        </p>   
+        <p className="flex justify-center items-center gap-1 text-sm">          
+          <footerIcons.copyright className="text-md"/> {currentYear} Charles Eimer - All rights reserved.
+        </p> 
+      </div>    
     </footer>
   );
 };
