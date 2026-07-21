c726918c125fa74067826fb9769e688d7e04abd2
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-07-21 16:48:29 -0400

    chore(router): remove unused ProjectPage import for ci build.

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from '../App';
 import HomePage from '../pages/HomePage';
-import ProjectPage from '../pages/ProjectPage';
 
 export const router = createBrowserRouter([
   {
